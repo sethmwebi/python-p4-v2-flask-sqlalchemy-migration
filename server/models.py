1230f1101087b8b8ca3255ef7d6fcabbb5dcb1c2
Seth Mwebi <sethmwebi27@gmail.com>
2024-10-08 12:07:08 +0300

    add flask-migrate

--- server/models.py
+++ server/models.py
@@ -11,11 +11,22 @@ db = SQLAlchemy(metadata=metadata)
 
 
 class Employee(db.Model):
-    __tablename__ = 'employees'
+    __tablename__ = "employees"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     salary = db.Column(db.Integer)
 
     def __repr__(self):
-        return f'<Employee {self.id}, {self.name}, {self.salary}>'
+        return f"<Employee {self.id}, {self.name}, {self.salary}>"
+
+
+class Department(db.Model):
+    __tablename__ = "departments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String)
+
+    def __repr__(self) -> str:
+        return f"<Department {self.id}, {self.name}, {self.address}>"
